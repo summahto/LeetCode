01af2433f83fa50368ce0fdf28f8024757704238
summahto <sumitkumarmahto3@gmail.com>
2021-10-27 16:04:29 +0000

    Code challenge submission for question: 344 Reverse String

---
+++ 344 Reverse String/344reverse-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        //0         
+        //Sumit
+        //
+        for(int i = 0; i< s.length/2 ; i++){
+            char temp = s[i];
+            s[i] = s[s.length - (i+1)];
+            s[s.length -i -1] = temp;
+        }
+        
+        
+    }
+}
