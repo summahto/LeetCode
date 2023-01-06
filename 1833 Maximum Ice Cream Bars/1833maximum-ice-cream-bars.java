bbef12329734404304d40a0abc73778a82c1d3a5
summahto <sumitkumarmahto3@gmail.com>
2023-01-06 04:11:54 +0000

    Code challenge submission for question: 1833 Maximum Ice Cream Bars

---
+++ 1833 Maximum Ice Cream Bars/1833maximum-ice-cream-bars.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        int count = 0;
+        Arrays.sort(costs);
+        for(int cost : costs){
+          if (coins - cost >= 0) {
+             coins -= cost;
+             count++;
+
+          }
+
+        }
+        return count;
+    }
+}
