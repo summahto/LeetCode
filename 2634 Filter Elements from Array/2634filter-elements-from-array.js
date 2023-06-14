4b275da76fb58614313aa386bac850f33d3e5e3d
summahto <sumitkumarmahto3@gmail.com>
2023-06-14 16:00:18 +0000

    Code challenge submission for question: 2634 Filter Elements from Array

---
+++ 2634 Filter Elements from Array/2634filter-elements-from-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    
+    const newArray = new Array();
+
+    for(let i =0; i< arr.length; i++){
+        
+        if(fn(arr[i], i)){
+            newArray.push(arr[i]);
+        }
+    }
+
+    return newArray;
+};
