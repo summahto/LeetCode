834bfc91468be21a97eefc5178d7f292782eb9c1
summahto <sumitkumarmahto3@gmail.com>
2025-01-22 10:20:08 -0500

    testing counters

--- PlusMult.py
+++ PlusMult.py
@@ -1,4 +1,4 @@
-
+from collections import Counter
 def plus_mult(nums: []):
 
     n = len(nums)
@@ -20,11 +20,11 @@ def plus_mult(nums: []):
                 r_odd += nums[j+2]
 
 
-
-
-
-
-
 if __name__ == '__main__':
     arr = {12, 3, 5, 7, 13, 12}
-    plus_mult(arr)
+
+    c1 = Counter("helllo")  # {'h': 1, 'e': 1, 'l': 3, 'o': 1}
+    c2 = Counter("hell")  # {'h': 1, 'e': 1, 'l': 2}
+
+    print(c1 - c2)
+    print(f"{not (c2 - c1)} =>if this is true it means c2 is a subseet of c1 ")
